96d42e15f861ab4f579cbabe4fd6f0182d4e8829
Henrik Lundstedt <hlundstedt@macadamian.com>
2014-05-22 11:18:45 -0400

    Revert "Revert "- Initial project setup""
    This reverts commit f07845ab6b01d3a2c51b80eddde6116d737b300e.

---
+++ Panoround/Panoround/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Panoround
+//
+//  Created by Henrik Lundstedt on 5/21/14.
+//  Copyright (c) 2014 Macadamian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
